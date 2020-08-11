ef03a32e325a33e097e443fe50a6480817df1207
Slattz <Slattz@users.noreply.github.com>
2020-08-11 04:33:22 +0100

    Fix All Items: Use game's function to add LBX part This calls one of the game's own functions to add a LBX item to the LBX array

--- Sources/cheats.cpp
+++ Sources/cheats.cpp
@@ -5,15 +5,33 @@
 
 //See savefile.bt for actual save structure
 enum Offsets : u32 {
-    Offsets_Items = 0x118
+    Offsets_Items = 0x118,
+    Offsets_LBXInv = 0xAE94,
+    Offsets_LBXItemArray = 0x12E8C
 };
 
 enum Sizes : u32 {
     Sizes_Item_Parts = 1956,
-    Sizes_BigItems = 6211,
-    Sizes_WeirdItems = 1589,
-    Sizes_Items_unk = 321
+    Sizes_BigItems = 5229,
+    Sizes_Unk1Items = 982,
+    Sizes_Unk2Items = 1589,
+    Sizes_Items_unk = 321,
+
+    Sizes_LBXItemArray = 0x1200,
 };
+
+struct PACKED LBXItem_s {
+    u16 Index;
+    u16 ItemID;
+    u16 unk_num;
+    u16 unk2_FFFF;
+    u16 Modifier1;
+    u16 Modifier2;
+    u16 Modifier3;
+    u16 Modifier4;
+    u16 unk5_FFFF;
+};
+static_assert(sizeof(LBXItem_s) == 0x12, "LBXItem_s is incorrect size");
 
 u32 GetSaveOffset() {
     u32 offset = 0;
@@ -21,6 +39,42 @@ u32 GetSaveOffset() {
         return offset;
 
     return 0;
+}
+
+u32 g_SetupAddLBXPartsAddr = 0x2BDBD8;
+u8 __attribute__((naked)) _SetupAddLBXParts(u32 unused, u32 Item, u32 unk_ptr) {
+    __asm__ __volatile__(
+        "STMFD  SP!, {R3-R12,LR}   	         \n\t" //Store All Registers except R0 (Unused), R1 (Item), R2 (unknown not required pointer)
+        "LDR   	R3, =g_SetupAddLBXPartsAddr  \n\t" //Set game address
+        "MOV    LR, PC					     \n\t" //Set return address (PC = (current instruction + 8) apparently)
+        "LDR    PC, [R3]				     \n\t" //Set PC to the game function. It stores LR so it will return to here+4
+        "LDMFD  SP!, {R3-R12,PC}   		     \n\t" //Restore All Registers
+    );
+}
+
+u8 SetupAddLBXParts(u32 Item) {
+    _SetupAddLBXParts(0, Item, 0);
+}
+
+void Setup_LBXItem_Inv() {
+    u32 bOffset = GetSaveOffset();
+    if (bOffset == 0) {
+        OSD::Notify(std::string(__PRETTY_FUNCTION__) + ": save offset was 0!");
+        return;
+    }
+
+    u32 orig[1] = {0};
+    Process::Patch(g_SetupAddLBXPartsAddr + 0x30, 0xEA000033, orig); //Fixes a game bug (lol) where it sets a memory address to 0 and continues the functions instead of returning from function
+
+    u32 off_itemTable = bOffset + 0x1062;
+    for (u32 i = 0; i < Sizes_BigItems; i++) {
+        u16 itemID = 0xFFFF;
+        Process::Read16(off_itemTable, itemID);
+        off_itemTable += 4; //sizeof(id)
+
+        SetupAddLBXParts(itemID);
+    }
+    Process::Patch(g_SetupAddLBXPartsAddr + 0x30, orig[0]); //Unpatch to keep original game code (unfix bug lol)
 }
 
 void AllItems(MenuEntry *entry) {
@@ -53,9 +107,20 @@ void AllItems(MenuEntry *entry) {
             Process::Write32(offset, val);
             offset += 4; //sizeof(val)
         }
+        Setup_LBXItem_Inv(); //Setup inventory for LBX items
 
-        //fill WeirdItems
-        for (u32 i = 0; i < Sizes_WeirdItems; i++) {
+        //fill Items_Unk1Items
+        for (u32 i = 0; i < Sizes_Unk1Items; i++) {
+            u32 val = 0;
+            Process::Read32(offset, val);
+            val &= 0xFFFF0000; //get rid of amount and BoughtStatus
+            val |= CompleteItem;
+            Process::Write32(offset, val);
+            offset += 4; //sizeof(val)
+        }
+
+        //fill Unk2_Items
+        for (u32 i = 0; i < Sizes_Unk2Items; i++) {
             u32 val = 0;
             Process::Read32(offset, val);
             val &= 0xFFFF0000; //get rid of amount and BoughtStatus
@@ -69,10 +134,6 @@ void AllItems(MenuEntry *entry) {
             Process::Write16(offset, CompleteItem);
             offset += 2; //sizeof(CompleteItem)
         }
-
-        #ifdef DEBUG
-        MessageBox(Utils::Format("Orig: %X, Current: %X\nExpected Diff: 0x8BA2\nActual Diff: %X", orig, offset, offset-orig))();
-        #endif
 
         OSD::Notify("All Items received!");
     }
@@ -179,4 +240,3 @@ void FriendExpGainEditor(MenuEntry *entry) {
         entry->Name() = Utils::Format("Friend EXP Multiplier (Current: x%d)", g_friendExpMultiplier);
     }
 }
-
